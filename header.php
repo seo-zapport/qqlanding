662ed5212d40b5f39d721c192ec385bd525d4619
jemsay07 <jem07.say@gmail.com>
2018-08-17 08:04:12 +0800

    Update header

--- header.php
+++ header.php
@@ -18,15 +18,17 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
-	<?php $layout = get_field( 'qqlanding_schema', 'option' );?>
+	<?php $layout = get_field( 'th_color_scheme', 'option' );?>
 </head>
 
 <body <?php body_class( 'qqlanding-sites qqland-' . qqlanding_schema( $layout ) ); ?>>
 <div id="page" class="site container<?php echo ( get_field( 'th_layout', 'option' ) == 'wide' ) ? '-full' : ''; ?>">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'qqlanding' ); ?></a>
+	
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<div class="container">
+				<?php echo $layout; ?>
 				<?php
 				the_custom_logo();
 				if ( is_front_page() && is_home() ) :
