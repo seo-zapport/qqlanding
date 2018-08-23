c021d4458362e29664e04ddf29ce389140dfa4f1
jemsay07 <11234073+jemsay07@users.noreply.github.com>
2018-08-23 10:15:03 +0800

    Update some changes - jem

--- inc/acf/acf-style-editor.php
+++ inc/acf/acf-style-editor.php
@@ -137,19 +137,7 @@ if ( have_rows( 'th_fonts', 'option' ) ) :
 		$font_style = get_sub_field('thr_font_style');
 		$font_weight = get_sub_field('thr_font_weight');
 
-		/*if ( get_sub_field('th_entry_item') == 'body' ) : ?>
-			body.qqlanding-sites{ font-family: <?php echo $font_family[0]; ?>; font-size: <?php echo $font_size; ?>px; font-style: <?php echo $font_style; ?>; font-weight: <?php echo $font_weight; ?>; }
-		<?php elseif( get_sub_field('th_entry_item') == 'content' ): ?>
-			.page-content,.entry-content,.entry-summary,.comment-content{ font-family: <?php echo $font_family[0]; ?>; font-size: <?php echo $font_size; ?>; font-style: <?php echo $font_style; ?>; font-weight: <?php echo $font_weight; ?>; }
-		<?php elseif( get_sub_field('th_entry_item') == 'meta' ): ?>
-			.genpost-entry-meta,.single-entry-meta,.genpost-entry-footer{ font-family: <?php echo $font_family[0]; ?>; font-size: <?php echo $font_size; ?>; font-style: <?php echo $font_style; ?>; font-weight: <?php echo $font_weight; ?>; }
-		<?php elseif( get_sub_field('th_entry_item') == 'link' ): ?>
-			a, .page-links{ font-family: <?php echo $font_family[0]; ?>; font-size: <?php echo $font_size; ?>; font-style: <?php echo $font_style; ?>; font-weight: <?php echo $font_weight; ?>; }
-		<?php else: ?>
-			.page-entry-title,.archive-page-title,.search-page-title,.entry-title{ font-family: <?php echo $font_family[0]; ?>; font-size: <?php echo $font_size; ?>; font-style: <?php echo $font_style; ?>; font-weight: <?php echo $font_weight; ?>; }
-		<?php endif;*/ ?>	
-
-		<?php if ( get_sub_field('th_entry_item') == 'body' ) : ?>
+		if ( get_sub_field('th_entry_item') == 'body' ) : ?>
 			body.qqlanding-sites{
 		<?php elseif( get_sub_field('th_entry_item') == 'content' ): ?>
 			.page-content,.entry-content,.entry-summary,.comment-content{
--- template-parts/content.php
+++ template-parts/content.php
@@ -35,18 +35,6 @@
 
 	<div class="entry-excerpt">
 		<?php
-		/*the_content( sprintf(
-			wp_kses(
-				 //translators: %s: Name of current post. Only visible to screen readers
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'qqlanding' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );*/
 		the_excerpt();
 
 		wp_link_pages( array(
