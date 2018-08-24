99f531d8a5b5c584ebf289ee610bfbc0c7032bf7
jemsay07 <11234073+jemsay07@users.noreply.github.com>
2018-08-24 17:07:18 +0800

    update 404 page by jem

--- 404.php
+++ 404.php
@@ -9,52 +9,22 @@
 
 get_header();
 ?>
-
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area col-md-12">
 		<main id="main" class="site-main">
-
-			<section class="error-404 not-found">
+			<section class="error-404 not-found text-center">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'qqlanding' ); ?></h1>
+					<h2 class="page-title" itemprop="headline"><?php esc_html_e( 'Oops!', 'qqlanding' ); ?></h2>
+					<h3 class="page-sub-title"><?php esc_html_e( '404', 'qqlanding' ); ?></h3>
 				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'qqlanding' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'qqlanding' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$qqlanding_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'qqlanding' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$qqlanding_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+				<div class="page-content text-center">
+					<h3><?php esc_html_e( 'That page can&rsquo;t be found.', 'qqlanding' ); ?></h3>
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try to search?', 'qqlanding' ); ?></p>
+					<di class="col-md-4 center-blocks"><?php get_search_form(); ?></di>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
 <?php
 get_footer();
