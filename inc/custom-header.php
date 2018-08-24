2501e192f5374eabc50c2f4c2ce6d1785518c4ab
jemsay07 <11234073+jemsay07@users.noreply.github.com>
2018-08-24 11:49:23 +0800

    Customizer & Acf functions update by jem

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -19,7 +19,7 @@
 function qqlanding_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'qqlanding_custom_header_args', array(
 		'default-image'          => '',
-		'default-text-color'     => '000000',
+		'default-text-color'     => 'FFFFFF',
 		'width'                  => 1000,
 		'height'                 => 250,
 		'flex-height'            => true,
---
+++ inc/customizer/customizer_style.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Customizer Site Colors
+ */
+
+function theme_site_colors(){
+	$theme_custom_style = '';
+	$theme_layout = get_field( 'th_layout', 'option' );
+
+	//echo $theme_layout;
+
+	$theme_wrap_color = get_theme_mod( 'qqLanding_wrap_color', '#f7f8f9');
+	$theme_text_color = get_theme_mod( 'qqLanding_theme_text_color', '#222222');
+
+	$theme_wrap_color = esc_attr( $theme_wrap_color );
+	$theme_text_color = esc_attr( $theme_text_color );
+
+	if ( $theme_layout === 'wide' && ! is_front_page() || $theme_layout === 'wide' && is_home() ) {
+		if ( $theme_wrap_color != '#f7f8f9' ) {
+			$theme_custom_style .= '.site-content{
+				background: '. $theme_wrap_color .';
+			}';
+		}
+	}
+
+	if ( $theme_layout === 'box' ) {
+		if ( $theme_wrap_color != '#f7f8f9' ) {
+			$theme_custom_style .= '.qqland-site-box.site{
+				background: '. $theme_wrap_color .';
+			}';
+		}
+	}
+
+	if( $theme_text_color != '#222222'  ){
+		$theme_custom_style .= '
+		.qqlanding-sites{
+			color: ' . $theme_text_color . ';
+		}
+		';
+	}
+
+	if ( ! empty($theme_custom_style) ) : ?>
+		<style type="text/css">
+			<?php echo $theme_custom_style; ?>
+		</style>
+	<?php endif;
+}
+add_action( 'wp_head', 'theme_site_colors');
